b569785a476bb322943b0a5da7f61653b3147c68
Emerson <140115328+emerson836@users.noreply.github.com>
2023-08-17 23:22:29 -0300

    alteracao

--- app.js
+++ app.js
@@ -19,33 +19,31 @@ buttons.forEach((btn) => {
 
 
     btn.addEventListener("click", () => {
-        // when clicked button is not erased button 
+
         if (!btn.id.match('erase')) {
-            // To display value on btn press
+
             realTimeScreenValue.push(btn.value)
             currentInput.innerHTML = realTimeScreenValue.join('');
 
-            // To evaluate answer in real time
+
             if (btn.classList.contains('num_btn')) {
                 answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
             }
         }
 
-        // When erase button is clicked
+
         if (btn.id.match('erase')) {
             realTimeScreenValue.pop();
             currentInput.innerHTML = realTimeScreenValue.join('');
             answerScreen.innerHTML = eval(realTimeScreenValue.join(''));
         }
 
-        // When clicked button is evaluate button
         if (btn.id.match('evaluate')) {
             currentInput.className = 'answerScreen';
             answerScreen.className = 'currentInput';
             answerScreen.style.color = "white";
         }
 
-        // To prevent undefined error in screen
         if (typeof eval(realTimeScreenValue.join('')) == 'undefined') {
             answerScreen.innerHTML = 0
         }
